3ca1a925aeebb05c02cca69d1d1e1c4c57cc240d
Pijush Ghorai <pghorai098@gmail.com>
2024-09-24 20:18:53 +0530

    project section

---
+++ app.js
@@ -0,0 +1,31 @@
+// Open close menu
+const sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+  sidemenu.style.right = "0px";
+}
+function closemenu() {
+  sidemenu.style.right = "-250px";
+}
+
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#05BFDB ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+
